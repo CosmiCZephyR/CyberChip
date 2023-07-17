699b07bb8f26eb438bcd79e9900cfc7afc6be33d
CosmiCZephyR <98467342+CosmiCZephyR@users.noreply.github.com>
2023-07-17 14:15:54 +0300

    Add files via upload

--- components/transistor/transistor.gd
+++ components/transistor/transistor.gd
@@ -10,18 +10,29 @@ func _ready():
 	_animation.seek(0.2)
 	body_entered.connect(self._interaction)
 
-func _interaction(_body):
+func _interaction(_body) -> void:
 	if _body.is_in_group("Player"):
-		Event.emit_signal("transistor_activated", self)
-		print_debug("Signal emmited")
+		#print_debug("Signal emmited")
+		Event.emit_signal("transistor_selected", self)
 
-func toggle():
+func toggle() -> void:
+	if !player_in_zone():
+		return
 	if is_on:
 		_animation.play("Off")
 		is_on = not is_on
 	else:
 		_animation.play_backwards("Off")
 		is_on = not is_on
+	Event.emit_signal("transistor_activated", self)
 
-func get_state():
+func get_state() -> bool:
 	return is_on
+
+func player_in_zone() -> bool:
+	var final_flag := false
+	var all_bodies = get_overlapping_bodies()
+	for body in all_bodies:
+		final_flag = final_flag || body.is_in_group("Player")
+	return final_flag
+	pass
