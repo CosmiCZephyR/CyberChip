db7203f19f3d1a58a6c54ed555c5d5c5fe6aa45f
CosmiCZephyR <98467342+CosmiCZephyR@users.noreply.github.com>
2023-07-18 18:07:41 +0300

    Добавлен прототип диода

---
+++ components/diode/diode.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+class_name Diode
+
+enum States {PERMANENT_LASER, VARIABLE_LASER, PERMANENT_LIGHT, VARIABLE_LIGHT}
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+# постоянный лазер
+func permanent_laser() -> void:
+	pass
+
+# переменный лазер
+func variable_laser() -> void:
+	pass
+
+# постоянный фонарь
+func permanent_light() -> void:
+	pass
+
+# пременный фонарь
+func variable_light() -> void:
+	pass
