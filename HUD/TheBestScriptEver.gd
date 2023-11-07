0467145d0371aa543730fb2dc56874a84c4160b6
CosmiCZephyR <zero.maiami@gmail.com>
2023-11-07 19:23:43 +0300

    Commit for merge

---
+++ HUD/TheBestScriptEver.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func _ready():
+	visible = true
--- InductionCoil.gd
+++ InductionCoil.gd
@@ -7,27 +7,28 @@ extends StaticBody2D
 
 var bodies: Array[Node2D]
 
+var visibility: bool = false
 var player_in_area: bool = false
+var last_point_pos: Vector2 = Vector2(0, 64)
 
 func _ready() -> void:
 	_timer.timeout.connect(_on_timer_timeout)
 
 func _process(_delta) -> void:
 	bodies = _area.get_overlapping_bodies()
-	
-	if player_in_area:
-		_line.points[5] = to_local(_player.global_position)
-	else:
-		_line.points[5] = Vector2(64, 0)
 
 func _on_timer_timeout() -> void:
+	_line.set_visible(visibility)
+	
 	for body in bodies:
+		_line.points[5] = last_point_pos
+		
 		if body.is_in_group("Player"):
-			player_in_area = true
 			create_lightning()
-		elif bodies.size() == 0:
-			player_in_area = false
 
 func create_lightning() -> void:
 	for point in range(1, _line.points.size() - 1):
 		_line.points[point].y = randi_range(-16, 16)
+	
+	visibility = true
+	last_point_pos = to_local(_player.global_position)
