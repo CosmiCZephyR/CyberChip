7834c9fce15c8d3e01d94195c06e8f80674d12c2
CosmiCZephyR <zero.maiami@gmail.com>
2023-11-30 22:44:35 +0300

    InventorySystem v0.71 + Godot 4.2 + Bug Fixes

--- InventorySystem/equipped_items_drag_input_handler.gd
+++ InventorySystem/equipped_items_drag_input_handler.gd
@@ -23,6 +23,8 @@ extends Control
 #
 #		return data
 
+@onready var _tilemap: TileMap = get_parent()
+
 func _can_drop_data(at_position, data) -> bool:
 	return can_drop_item(at_position, data)
 
@@ -33,7 +35,7 @@ func _drop_data(at_position, data) -> void:
 	drop_item(at_position, data)
 
 func drop_item(at_position, data) -> void:
-	var scene = data["item_scene"]
+	var scene = data.item_scene
 	var scene_instance = scene.instantiate()
-	scene_instance.position = get_local_mouse_position() as Vector2i
+	scene_instance.position = _tilemap.map_to_local(_tilemap.local_to_map(get_local_mouse_position()))
 	add_child(scene_instance)
